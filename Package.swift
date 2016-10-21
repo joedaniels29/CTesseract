be4d828709f7a50652faf07c0b43d588b4527a32
Joseph Daniels <joedaniels29@me.com>
2016-10-21 10:21:18 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Apollo
+//
+//  Created by Joseph Daniels on 21/10/2016.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Ruby",
+    pkgConfig:"ruby-2.4",
+)
