ea0515529d20856e6ce9845f8ea8ce1c82d44738
Joseph Daniels <joedaniels29@me.com>
2016-10-22 12:04:24 -0400

    Fix header Location

---
+++ Sources/rswift.h
@@ -0,0 +1,18 @@
+//
+//  Apollo.h
+//  Apollo
+//
+//  Created by Joseph Daniels on 19/10/2016.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Apollo.
+FOUNDATION_EXPORT double ApolloVersionNumber;
+
+//! Project version string for Apollo.
+FOUNDATION_EXPORT const unsigned char ApolloVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <Apollo/PublicHeader.h>
